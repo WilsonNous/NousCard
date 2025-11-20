c5633c5f45fb1039d52c1d14ae6972bd58961972
WilsonNous <107373913+WilsonNous@users.noreply.github.com>
2025-11-19 22:12:59 -0300

    Create usuario.py

---
+++ models/usuario.py
@@ -0,0 +1,18 @@
+from .base import db
+from datetime import datetime
+
+class Usuario(db.Model):
+    __tablename__ = "usuarios"
+
+    id = db.Column(db.Integer, primary_key=True)
+    empresa_id = db.Column(db.Integer, db.ForeignKey("empresas.id"), nullable=False)
+
+    nome = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    senha_hash = db.Column(db.String(255), nullable=False)
+
+    admin = db.Column(db.Boolean, default=False)
+    criado_em = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Usuario {self.email}>"
