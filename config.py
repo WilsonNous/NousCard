6ba907bae20c6685bacc3ddc924e4411d977ca01
WilsonNous <107373913+WilsonNous@users.noreply.github.com>
2025-11-20 09:35:04 -0300

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ import os
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key-nouscard")
 
-    SSQLALCHEMY_DATABASE_URI = "mysql+pymysql://noust785_nouscard_user:n0usc%40rd@108.167.132.58/noust785_nouscard_db"
+    SQLALCHEMY_DATABASE_URI = (
+        "mysql+pymysql://noust785_nouscard_user:n0usc%40rd@108.167.132.58/noust785_nouscard_db"
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
