cbc28c3539dfb51f296888f4616802765c7f0e38
WilsonNous <107373913+WilsonNous@users.noreply.github.com>
2025-11-19 22:26:02 -0300

    Update contrato_routes.py

--- routes/contrato_routes.py
+++ routes/contrato_routes.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, jsonify
+from utils.auth_middleware import login_required
 
 contrato_bp = Blueprint("contrato", __name__)
 
 @contrato_bp.route("/taxas", methods=["GET"])
+@login_required
 def listar_contratos():
     # TODO: retornar contratos reais
     return jsonify({"ok": True, "items": []})
