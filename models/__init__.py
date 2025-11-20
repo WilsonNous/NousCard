1ed7c0bbec5fe76571141896751f4a3b1d02cd3e
WilsonNous <107373913+WilsonNous@users.noreply.github.com>
2025-11-19 22:13:42 -0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -6,3 +6,4 @@ from .contrato_taxa import ContratoTaxa
 from .mov_adquirente import MovAdquirente
 from .mov_banco import MovBanco
 from .conciliacao import Conciliacao
+from .usuario import Usuario
