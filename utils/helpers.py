486b66eb15728cdc5aecf811257abb16d2589756
WilsonNous <107373913+WilsonNous@users.noreply.github.com>
2025-11-21 10:33:05 -0300

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,3 +1,10 @@
+import hashlib
+
+def gerar_hash_arquivo(file_storage):
+    conteudo = file_storage.read()
+    file_storage.seek(0)  # reset para permitir leitura depois
+    return hashlib.sha256(conteudo).hexdigest()
+    
 def format_currency_br(value: float) -> str:
     try:
         value = float(value)
